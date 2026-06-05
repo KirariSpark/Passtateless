a64a742b959bfe00019ae9464a58bc643383d5f3
XJF2332 <xujianfeng233@163.com>
2026-06-05 18:31:12 +0800

    优化：密码编辑UI

--- lib/ui/pages/pwd/edit.dart
+++ lib/ui/pages/pwd/edit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:passtateless/modules/core/error_codes.dart';
 import 'package:passtateless/modules/core/logger.dart';
+import 'package:passtateless/modules/providers/app_provider.dart';
 import 'package:passtateless/modules/providers/pwd_provider.dart';
 import 'package:passtateless/modules/utils/ui.dart' as ui;
 import 'package:passtateless/ui/styles.dart' as styles;
@@ -11,6 +12,7 @@ import 'package:provider/provider.dart';
 class PwdEditPage extends StatefulWidget {
   /// 要编辑的密码条目的UUID
   final String id;
+
   const PwdEditPage({super.key, required this.id});
 
   @override
@@ -18,16 +20,21 @@ class PwdEditPage extends StatefulWidget {
 }
 
 class _PwdEditPageState extends State<PwdEditPage> {
-  late TextEditingController _identifierController;
-  late TextEditingController _userNameController;
-  late TextEditingController _accountController;
+  late final TextEditingController _identifierController;
+  late final TextEditingController _userNameController;
+  late final TextEditingController _accountController;
+  late final PwdProvider _pwdProvider;
+  late final AppProvider _appProvider;
+
 
   @override
   void initState() {
-    appLogger.logger.i("Editing password id ${widget.id}");
     super.initState();
-    final pwdProvider = Provider.of<PwdProvider>(context, listen: false);
-    final data = pwdProvider.getItemById(widget.id);
+    appLogger.logger.i("Editing password id ${widget.id}");
+    _pwdProvider = context.read<PwdProvider>();
+    _appProvider = context.read<AppProvider>();
+
+    final data = _pwdProvider.getItemById(widget.id);
     _identifierController = TextEditingController(text: data["identifier"]);
     _userNameController = TextEditingController(text: data["userName"]);
     _accountController = TextEditingController(text: data["account"]);
@@ -41,77 +48,67 @@ class _PwdEditPageState extends State<PwdEditPage> {
     super.dispose();
   }
 
+  AppBar? _buildAppBar() {
+    return styled.buildAppBar(
+      title: "编辑：${_identifierController.text == '' ? '未命名' : _identifierController.text}",
+      context: context,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final pwdProvider = Provider.of<PwdProvider>(context, listen: false);
-
     return Scaffold(
-      appBar: styled.buildAppBar(
-        title: "编辑：${_identifierController.text == '' ? '未命名' : _identifierController.text}",
-        context: context,
-      ),
+      appBar: _buildAppBar(),
       body: SingleChildScrollView(
         child: Container(
           alignment: Alignment.topCenter,
-          padding: styles.uniInsetsSmall,
-          child: Column(
-            spacing: 8,
-            children: <Widget>[
-              // 文本框区域
-              Column(
-                spacing: styles.layoutSpacing,
-                children: [
-                  // 档案名
-                  ConstrainedBox(
-                    constraints: styles.tileWidthConstraint,
-                    child: styled.buildTextField(
-                      context: context,
-                      controller: _identifierController,
-                      onChanged: (value) {
-                        final res = pwdProvider.setValueById(widget.id, "identifier", value);
-                        if (res != ErrorCode.success) {
-                          appLogger.logger.e("Failed to rename: $res");
-                          ui.showSnackBarQuick(res.generic, context);
-                        }
-                      },
-                      label: "档案名",
-                    ),
-                  ),
-                  // 用户名
-                  ConstrainedBox(
-                    constraints: styles.tileWidthConstraint,
-                    child: styled.buildTextField(
-                      context: context,
-                      controller: _userNameController,
-                      onChanged: (value) {
-                        final res = pwdProvider.setValueById(widget.id, "userName", value);
-                        if (res != ErrorCode.success) {
-                          appLogger.logger.e("Cannot change userName: $res");
-                          ui.showSnackBarQuick(res.generic, context);
-                        }
-                      },
-                      label: "用户名",
-                    ),
-                  ),
-                  // 账号
-                  ConstrainedBox(
-                    constraints: styles.tileWidthConstraint,
-                    child: styled.buildTextField(
-                      context: context,
-                      controller: _accountController,
-                      onChanged: (value) {
-                        var res = pwdProvider.setValueById(widget.id, "account", value);
-                        if (res != ErrorCode.success) {
-                          appLogger.logger.e("Cannot change account: $res");
-                          ui.showSnackBarQuick(res.generic, context);
-                        }
-                      },
-                      label: "账号",
-                    ),
-                  ),
-                ],
-              )
-            ],
+          padding: styles.pagePaddingAll,
+          child: ConstrainedBox(
+            constraints: styles.tileWidthConstraint,
+            child: Column(
+              spacing: styles.layoutSpacing,
+              children: <Widget>[
+                styled.buildTextField(
+                  context: context,
+                  controller: _identifierController,
+                  onChanged: (value) {
+                    _appProvider.hasUnsavedChanges = true;
+                    final stat = _pwdProvider.setValueById(widget.id, "identifier", value);
+                    if (stat != ErrorCode.success) {
+                      appLogger.logger.e("Failed to rename archive ${widget.id}: $stat");
+                      ui.showSnackBarQuick(stat.generic, context);
+                    }
+                  },
+                  label: "档案名",
+                ),
+                styled.buildTextField(
+                  context: context,
+                  controller: _userNameController,
+                  onChanged: (value) {
+                    _appProvider.hasUnsavedChanges = true;
+                    final stat = _pwdProvider.setValueById(widget.id, "userName", value);
+                    if (stat != ErrorCode.success) {
+                      appLogger.logger.e("Cannot change userName for archive ${widget.id}: $stat");
+                      ui.showSnackBarQuick(stat.generic, context);
+                    }
+                  },
+                  label: "用户名",
+                ),
+                styled.buildTextField(
+                  context: context,
+                  controller: _accountController,
+                  onChanged: (value) {
+                    _appProvider.hasUnsavedChanges = true;
+                    final stat = _pwdProvider.setValueById(widget.id, "account", value);
+                    if (stat != ErrorCode.success) {
+                      appLogger.logger.e("Cannot change account for archive ${widget.id}: $stat");
+                      ui.showSnackBarQuick(stat.generic, context);
+                    }
+                  },
+                  label: "账号",
+                )
+              ],
+            ),
           ),
         ),
       ),
