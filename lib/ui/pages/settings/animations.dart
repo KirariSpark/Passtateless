c1f16d9f15d543a357a7c05d71d37ae3f69ea236
XJF2332 <xujianfeng233@163.com>
2026-06-09 17:04:19 +0800

    优化：动画设置页面代码结构

--- lib/ui/pages/settings/animations.dart
+++ lib/ui/pages/settings/animations.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:passtateless/modules/core/enums.dart';
 import 'package:passtateless/modules/core/error_codes.dart';
+import 'package:passtateless/modules/providers/app_provider.dart';
 import 'package:passtateless/modules/utils/ui.dart' as ui;
 import 'package:passtateless/ui/styles.dart' as styles;
 import 'package:passtateless/ui/widgets/styled.dart' as styled;
-import 'package:passtateless/modules/providers/app_provider.dart';
 import 'package:provider/provider.dart';
 
 class AnimationSettingsPage extends StatefulWidget {
   /// 有AppBar时，是否要使用Hero动画
   final bool useHero;
+
   /// 是否要包含AppBar
   final bool hasAppBar;
+
   /// 是否有内边距
   final bool hasPadding;
+
   const AnimationSettingsPage({super.key, required this.useHero, this.hasAppBar = true, this.hasPadding = true});
 
   @override
@@ -21,10 +24,17 @@ class AnimationSettingsPage extends StatefulWidget {
 }
 
 class _AnimationSettingsPageState extends State<AnimationSettingsPage> {
+  late final AppProvider appProviderRead;
+
+  @override
+  void initState() {
+    super.initState();
+    appProviderRead = context.read<AppProvider>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final AppProvider notListen = Provider.of<AppProvider>(context, listen: false);
-    final AppProvider listen = Provider.of<AppProvider>(context);
+    final AppProvider appProviderWatch = context.watch<AppProvider>();
 
     return Scaffold(
       appBar: widget.hasAppBar
@@ -32,21 +42,20 @@ class _AnimationSettingsPageState extends State<AnimationSettingsPage> {
         : null,
       body: Container(
         alignment: Alignment.topCenter,
-        child: Container(
-          padding: widget.hasPadding ? styles.pagePaddingAll : EdgeInsets.zero,
+        padding: widget.hasPadding ? styles.pagePaddingAll : EdgeInsets.zero,
+        child: ConstrainedBox(
           constraints: styles.tileWidthConstraint,
           child: SingleChildScrollView(
             child: Column(
               spacing: styles.layoutSpacing,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // 动画风格
                 Text("动画风格", style: Theme.of(context).textTheme.titleLarge),
                 RadioGroup(
-                  groupValue: listen.currentNavMode,
+                  groupValue: appProviderWatch.currentNavMode,
                   onChanged: (value) async {
-                    notListen.currentNavMode = value!;
-                    final stat = await notListen.saveConfig();
+                    appProviderRead.currentNavMode = value!;
+                    final stat = await appProviderRead.saveConfig();
                     if (stat != ErrorCode.success && context.mounted) {
                       ui.showSnackBarQuick(stat.generic, context);
                     }
@@ -67,13 +76,15 @@ class _AnimationSettingsPageState extends State<AnimationSettingsPage> {
                   )
                 ),
                 Divider(),
-                // 动画速度
                 Text("动画速度", style: Theme.of(context).textTheme.titleLarge),
                 RadioGroup(
-                  groupValue: listen.currentDilation,
-                  onChanged: (value) {
-                    notListen.currentDilation = value!;
-                    notListen.saveConfig();
+                  groupValue: appProviderWatch.currentDilation,
+                  onChanged: (value) async {
+                    appProviderRead.currentDilation = value!;
+                    final stat = await appProviderRead.saveConfig();
+                    if (stat != ErrorCode.success && context.mounted) {
+                      ui.showSnackBarQuick(stat.generic, context);
+                    }
                   },
                   child: Column(
                     children: [
@@ -81,11 +92,11 @@ class _AnimationSettingsPageState extends State<AnimationSettingsPage> {
                         value: item,
                         title: Text(item.displayName),
                         tileColor: ColorScheme.of(context).surfaceContainerLow,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: ui.calcRadius(
-                              isFirst: index == 0, isLast: index == AnimationDilation.values.length - 1
-                            ),
-                          )
+                        shape: RoundedRectangleBorder(
+                          borderRadius: ui.calcRadius(
+                            isFirst: index == 0, isLast: index == AnimationDilation.values.length - 1
+                          ),
+                        )
                       )
                     ],
                   )
