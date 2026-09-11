7b2baf696b30d5eaa731bb9ccc7ecad6b0dcbce0
KirariSpark <xujianfeng233@163.com>
2026-09-11 23:06:57 +0800

    Bump Version

--- lib/ui/pages/settings/about.dart
+++ lib/ui/pages/settings/about.dart
@@ -26,7 +26,7 @@ class AboutPage extends StatelessWidget {
                 Image.asset("assets/icon.png", width: 100),
                 styles.spacingSizedBox,
                 Text("Passtateless", style: Theme.of(context).textTheme.bodyLarge),
-                Text("0.2.1 - beta"),
+                Text("0.2.2 - beta"),
                 styles.spacingSizedBox,
                 styled.buildTextButton(
                   onPressed: () => showLicensePage(context: context),
